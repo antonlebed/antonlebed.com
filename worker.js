11ff193ceb537a4dd3be7cee550fea1efb4b509b
Anton Lebed <coglebed@gmail.com>
2026-03-22 20:49:52 -0400

    Deploy: 2026-03-22_20:49

--- worker.js
+++ worker.js
@@ -240,7 +240,7 @@ export default {
         '<\/script>';
 
       /* Full HTML: <head> + rendered body + bootstrap script */
-      var html = '<!DOCTYPE html><html lang="en" style="background:#050508;margin:0;padding:0;min-height:100vh;width:100%"><head>' + head + '</head>' + render(0) + boot + '</html>';
+      var html = '<!DOCTYPE html><html lang="en"><head>' + head + '</head>' + render(0) + boot + '</html>';
 
       return new Response(html, {
         headers: { 'Content-Type': 'text/html;charset=utf-8' }
