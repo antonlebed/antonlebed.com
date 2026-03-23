8b72b01b4720f942a8cb00c83636c00bb274f5d7
Anton Lebed <coglebed@gmail.com>
2026-03-23 13:34:12 -0400

    Deploy: 2026-03-23_13:34

--- public/webax/build_native.js
+++ public/webax/build_native.js
@@ -29,7 +29,7 @@ const FILES = [
   'page_stormer_pairs.ax', 'page_shadow_eval.ax', 'page_d_power_gaussian.ax',
   'page_cyclotomic_fibonacci.ax', 'page_fano_e8.ax', 'page_arcsine_cumulant.ax',
   'page_figurate_bridge.ax',
-  'page_heart.ax', 'page_sandpile.ax', 'page_swim.ax', 'page_repl.ax', 'page_prerequisites.ax', 'page_tutorial.ax', 'pages_observe.ax', 'pages_living.ax',
+  'page_heart.ax', 'page_sandpile.ax', 'page_swim.ax', 'page_repl.ax', 'page_prerequisites.ax', 'page_tutorial.ax', 'page_arc.ax', 'pages_observe.ax', 'pages_living.ax',
   'router.ax'
 ];
 
